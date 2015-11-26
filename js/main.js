37a1dc9bb196816a47f832b81692a0d523713738
Ravenstorm Labarcon <ravenstormmlabarcon@gmail.com>
2015-11-26 14:02:35 -0800

    update

--- js/main.js
+++ js/main.js
@@ -2,25 +2,34 @@
 
 $(document).ready(function() {
 
-    //settings for slider
-    var width = 1067;
-    var animationSpeed = 1000;
-    var pause = 4000;
-    var currentSlide = 1;
+        //settings for slider
+        
+        var animationSpeed = 1000;
+        var pause = 4000;
+        var currentSlide = 1;
 
-    //cache DOM elements
-    var $slider = $('#slider');
-    var $slideContainer = $('.slides', $slider);
-    var $slides = $('.slide', $slider);
+        //cache DOM elements
+        var $slider = $('#slider');
+        var $slideContainer = $('.slides', $slider);
+        var $slides = $('.slide', $slider);
 
-    $('.button').click(function(){
-    	$slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function() {
-                if (++currentSlide === $slides.length) {
-                    currentSlide = 1;
-                    $slideContainer.css('margin-left', 0);
-                }
-            });
-    });
+        $('.button').click(function(){
+
+            var currentWidth = $('.slide img').width();
+
+            $('#slider .slides').css({"width": (currentWidth * 5.1)});
+
+        	$slideContainer.animate({'margin-left': '-='+currentWidth}, animationSpeed, function() {
+                    if (++currentSlide === $slides.length) {
+                        currentSlide = 1;
+                        $slideContainer.css('margin-left', 0);
+                    }
+                });
+        }); 
+
+        $(window).resize(function () {
+            $slideContainer.css('margin-left', 0);
+        });
 
 
 });
